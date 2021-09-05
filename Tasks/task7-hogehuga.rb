d4a05ac693e21b4b5f3d21adf2e56a6fe00b3fb8
RouxRhett <nsch.4871@gmail.com>
2021-09-05 12:32:34 +0900

    コメントを追加しました。

--- Tasks/task7-hogehuga.rb
+++ Tasks/task7-hogehuga.rb
@@ -1,14 +1,20 @@
+#整数が入力されるまでループします。
 while true do
+  #標準入力を求めます。
   puts "整数を入力してください。"
   input = gets
+  #文字列の先頭から末尾まで数値で構成されているか判定します
   if input =~ /^[0-9]+$/
+    #入力値を2で割った余りによって出力を変更します。
     if input % 2 == 0
       puts "hoge"
     else
       puts "huga"
     end
+    #整数が入力されればbreakでループを抜けます。
     break
   else
+    #整数以外を入力されると再度入力を求めるように出力します。
     puts "Error!!"
   end
 end
